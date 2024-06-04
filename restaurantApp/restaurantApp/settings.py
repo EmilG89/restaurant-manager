dd52f317889e45de39fc057065dc4aef44fec34c
Emil <131275663+EmilG89@users.noreply.github.com>
2024-06-05 00:49:01 +0300

    Update settings.py

--- restaurantApp/restaurantApp/settings.py
+++ restaurantApp/restaurantApp/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restaurantApp.restaurantManager.apps.RestaurantmanagerConfig'
+    'restaurantManager.apps.RestaurantmanagerConfig'
 ]
 
 MIDDLEWARE = [
