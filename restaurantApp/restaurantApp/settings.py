54f0f3cf7898731e4c174994800c55875be39e2e
Emil <131275663+EmilG89@users.noreply.github.com>
2024-06-04 19:13:27 +0300

    Update settings.py
    update database settings

--- restaurantApp/restaurantApp/settings.py
+++ restaurantApp/restaurantApp/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     #    'NAME': 'restaurant_db_ggh2',
     #    'USER': 'restaurant_db_ggh2_user',
     #    'PASSWORD': 'EvGXHxTRmn7Znc3AnyCPawjjw5dtfRHB',
-    #    'HOST': dj_database_url.config(default=os.environ.get('postgres://restaurant_db_ggh2_user:EvGXHxTRmn7Znc3AnyCPawjjw5dtfRHB@dpg-cpet2mf109ks73fk7v0g-a.frankfurt-postgres.render.com/restaurant_db_ggh2')),
+    #    'HOST': 'dpg-cpet2mf109ks73fk7v0g-a.frankfurt-postgres.render.com',
     #    'PORT': '5432',
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
