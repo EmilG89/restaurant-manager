f1146b17d42cd19a560610440e028518aba59243
Emil <131275663+EmilG89@users.noreply.github.com>
2024-06-04 22:10:43 +0300

    Update asgi.py
    update settings path

--- restaurantApp/restaurantApp/asgi.py
+++ restaurantApp/restaurantApp/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurantApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurantApp.restaurantApp.settings')
 
 application = get_asgi_application()
