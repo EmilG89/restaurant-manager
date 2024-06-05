6e93fbd67a4380f685e978de48eb3c1552ca3d46
Emil <131275663+EmilG89@users.noreply.github.com>
2024-06-05 10:08:30 +0300

    Update wsgi.py

--- restaurantApp/restaurantApp/wsgi.py
+++ restaurantApp/restaurantApp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurantApp.restaurantApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurantApp.settings')
 
 application = get_wsgi_application()
